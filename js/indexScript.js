9689f2dd62f2d0044a929a5f1d68413f362ea4a8
Tomas Casa WIn <210099@fppiramide.com>
2022-11-26 17:37:36 +0100

    tuneo y ranita

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,1 +1,12 @@
-window.alert("Quiero hacerme viejo de besito en besito \n tu bicho");
+// window.alert("Quiero hacerme viejo de besito en besito \n tu bicho");
+
+let rana = document.getElementsByClassName("mensaje_rana")[0];
+let cubierta = document.getElementsByClassName("cubierta")[0];
+
+rana.addEventListener("click", () => {
+    rana.classList.add("desaparece");
+    rana.classList.remove("mensaje_rana");
+
+    cubierta.classList.remove("cubierta");
+    cubierta.classList.add("desaparece");
+})
